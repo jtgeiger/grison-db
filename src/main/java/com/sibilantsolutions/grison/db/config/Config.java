3ef401093126840988b93cab7fd5a6934cd4c6f5
Joel Geiger <joel@sibilantsolutions.com>
2016-03-09 21:45:45 -0500

    Use spring-boot-starter-data-rest to enable HATEOAS interface.
    Currently experimental, to see what it can do and decide whether to keep it.

--- src/main/java/com/sibilantsolutions/grison/db/config/Config.java
+++ src/main/java/com/sibilantsolutions/grison/db/config/Config.java
@@ -1,14 +1,24 @@
 package com.sibilantsolutions.grison.db.config;
 
 import com.sibilantsolutions.grison.db.handler.FileWriterParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
 
-@Configuration
+//Customize this object's bean name away from default generated name of "config" to avoid conflict with
+//org.springframework.boot.autoconfigure.data.rest.SpringBootRepositoryRestMvcConfiguration.config()
+@Configuration("myConfig")
 public class Config {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Config.class);
+
+    Config() {
+        LOG.info("Constructed {}.", getClass());
+    }
 
     @Bean
     FileWriterParams imageFileWriterParams(@Value("${imageFileDir}") String dir) {
