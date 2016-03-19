138af1236fcab0c719151e0b337fe9bafb542f74
Joel Geiger <joel@sibilantsolutions.com>
2016-03-19 11:36:22 -0400

    Add an example query method: findByConnectTimeGreaterThanEqual.

--- src/main/java/com/sibilantsolutions/grison/db/persistence/repository/CamSessionRepository.java
+++ src/main/java/com/sibilantsolutions/grison/db/persistence/repository/CamSessionRepository.java
@@ -2,6 +2,13 @@ package com.sibilantsolutions.grison.db.persistence.repository;
 
 import com.sibilantsolutions.grison.db.persistence.entity.CamSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 public interface CamSessionRepository extends JpaRepository<CamSession, Long> {
+
+    List<CamSession> findByConnectTimeGreaterThanEqual(@Param("start") Timestamp start);
+
 }
